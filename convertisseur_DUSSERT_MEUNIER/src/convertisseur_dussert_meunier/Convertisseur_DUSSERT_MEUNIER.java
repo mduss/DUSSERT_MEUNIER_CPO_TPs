475e2863fd66491e19d35c8527c171f5ee4b1e8f
Marine <Marine@192.168.115.152>
2021-10-04 11:55:59 +0200

    Initialisation conv

---
+++ convertisseur_DUSSERT_MEUNIER/src/convertisseur_dussert_meunier/Convertisseur_DUSSERT_MEUNIER.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package convertisseur_dussert_meunier;
+
+/**
+ *
+ * @author Marine
+ */
+public class Convertisseur_DUSSERT_MEUNIER {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Partie 1 :
+        
+    }
+    
+}
