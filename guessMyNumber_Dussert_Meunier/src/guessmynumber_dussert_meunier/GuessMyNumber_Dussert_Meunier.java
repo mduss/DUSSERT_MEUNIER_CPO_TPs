f27d119b0ceda64ee399c1e73b877d17a40e0b10
Marine <Marine@192.168.115.152>
2021-10-06 16:45:56 +0200

    création des niveaux de difficultés du jeu

--- guessMyNumber_Dussert_Meunier/src/guessmynumber_dussert_meunier/GuessMyNumber_Dussert_Meunier.java
+++ guessMyNumber_Dussert_Meunier/src/guessmynumber_dussert_meunier/GuessMyNumber_Dussert_Meunier.java
@@ -4,6 +4,9 @@
  */
 package guessmynumber_dussert_meunier;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author Marine
@@ -15,6 +18,59 @@ public class GuessMyNumber_Dussert_Meunier {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Random generateurAleat = new Random();
+        Scanner sc=new Scanner(System.in);
+        
+        System.out.println("""
+                           1) Facile
+                           2) Normal
+                           3) Difficile
+                           4) Cauchemar
+                           
+                           Saisissez le numéro du niveau choisi :
+                           """);
+        int niv = sc.nextInt(); 
+        
+        if (1==niv) {
+            int n = generateurAleat.nextInt(50);
+            int saisi = sc.nextInt(); 
+            int compt = 0;
+            while (saisi!=n) {
+                saisi = sc.nextInt();
+                if (saisi<n) {
+                    System.out.println("Trop petit");
+                }
+                else if (saisi>n) {
+                    System.out.println("Trop grand");
+
+                }
+                compt+=1;
+            }
+        }
+        
+        else if (2==niv){
+            int n = generateurAleat.nextInt(100);
+            int saisi = sc.nextInt(); 
+            int compt = 0;
+            while (saisi!=n) {
+                saisi = sc.nextInt();
+                if (saisi<n) {
+                    System.out.println("Trop petit");
+                }
+                else if (saisi>n) {
+                    System.out.println("Trop grand");
+                }
+                compt+=1;
+            }
+        }
+        
+        
+        System.out.println("Gagné à la tentative numéro " + compt);
+        
+        }
+     
     }
+
+        
     
 }
