9677a2ea76ac99fb7e8c7fc9270a88584dc90fd2
dusse <dusse@LAPTOP-0NTV7NA1>
2021-10-18 19:46:48 +0200

    TP2 bière en cours

--- TP2_Bieres_DUSSERT_MEUNIER/src/tp2_bieres_dussert_meunier/BouteilleBiere.java
+++ TP2_Bieres_DUSSERT_MEUNIER/src/tp2_bieres_dussert_meunier/BouteilleBiere.java
@@ -15,7 +15,25 @@ public class BouteilleBiere {
     String brasserie;
     Boolean ouverte;
     
+    public BouteilleBiere(String unNom, double unDegre, String uneBrasserie) {
+        Nom = unNom;
+        degreAlcool = unDegre;
+        brasserie = uneBrasserie;
+        ouverte = false;
+    }
+    
     public void lireEtiquette() {
         System.out.println("Bouteille de " + Nom +" (" + degreAlcool +" degres) \nBrasserie : " + brasserie ) ;
+    }
+    
+    public boolean Décapsuler() {
+        if (ouverte==false){
+            ouverte=true;
+            return true;
+        }
+        else {
+            System.out.println("erreur : bière déja ouverte");
+            return false;
+        }
     }
 }
