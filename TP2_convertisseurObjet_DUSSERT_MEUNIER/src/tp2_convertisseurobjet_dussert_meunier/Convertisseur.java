a4d3273283931fdec33625fd5e72b48b0719f989
dusse <dusse@192.168.1.100>
2021-11-01 16:51:41 +0100

    TP2 convertisseur 5) fini

--- TP2_convertisseurObjet_DUSSERT_MEUNIER/src/tp2_convertisseurobjet_dussert_meunier/Convertisseur.java
+++ TP2_convertisseurObjet_DUSSERT_MEUNIER/src/tp2_convertisseurobjet_dussert_meunier/Convertisseur.java
@@ -13,29 +13,29 @@ package tp2_convertisseurobjet_dussert_meunier;
 public class Convertisseur {
     int nbConversions=0;
     
-    public static double CelciusVersKelvin (double tempCelcius) {     
+    public double CelciusVersKelvin (double tempCelcius) {     
         double c =tempCelcius+273;
-        return c;
+        return c ;
     }
     
-    public static double KelvinVersCelcius (double tempKelvin) {     
+    public double KelvinVersCelcius (double tempKelvin) {     
         double c =tempKelvin-273;
         return c;
     }
     
-    public static double FarenheitVersCelcius (double tempFara) {     
+    public double FarenheitVersCelcius (double tempFara) {     
         double c =(tempFara-32)*(5.0/9);
         return c;
     }
-    public static double CelciusVersFarenheit (double tempCelcius) {     
+    public double CelciusVersFarenheit (double tempCelcius) {     
         double c =tempCelcius*(9/5.0)+32;
         return c;
     }
-    public static double KelvinVersFarenheit (double tempKelvin) {     
+    public double KelvinVersFarenheit (double tempKelvin) {     
         double c =(tempKelvin-273)*(9/5.0)+32;
         return c;
     }
-    public static double FarenheitVersKelvin (double tempFara) {     
+    public double FarenheitVersKelvin (double tempFara) {     
         double c =(tempFara-32)*(9/5.0)+273;
         return c;
     }
--- TP2_convertisseurObjet_DUSSERT_MEUNIER/src/tp2_convertisseurobjet_dussert_meunier/TP2_convertisseurObjet_DUSSERT_MEUNIER.java
+++ TP2_convertisseurObjet_DUSSERT_MEUNIER/src/tp2_convertisseurobjet_dussert_meunier/TP2_convertisseurObjet_DUSSERT_MEUNIER.java
@@ -4,6 +4,7 @@
  */
 package tp2_convertisseurobjet_dussert_meunier;
 
+import java.util.Scanner;
 /**
  *
  * @author Marine
@@ -14,13 +15,80 @@ public class TP2_convertisseurObjet_DUSSERT_MEUNIER {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        int a = 100;
+        // TOD9O code application logic here
+        Scanner sc=new Scanner(System.in);
+        
+        int a=100;
         Convertisseur objet1 = new Convertisseur();
         Convertisseur objet2 = new Convertisseur();
         
-        System.out.println(objet1);
-        System.out.println(objet2);
+        System.out.println("""
+                           Saisissez les conversions que vous souhaitez effectuer pour objet 1:
+                           1) De Celcius vers Kelvin
+                           2) De Kelvin vers Celcius
+                           3) De Farenheit vers Celcius
+                           4) De Celcius vers Farenheit
+                           5) De Kelvin vers Farenheit
+                           6) De Farenheit vers Kelvin
+                           """);
+        int convert = sc.nextInt();
+        while (1<=convert && convert<=6){
+            if (1 == convert) {
+                objet1.CelciusVersFarenheit(a);
+            }
+            else if (2==convert) {
+                objet1.KelvinVersCelcius(a);
+            }       
+            else if (3==convert) {
+                objet1.FarenheitVersCelcius(a);
+            }       
+            else if (4==convert) {
+                objet1.CelciusVersFarenheit(a);
+            }   
+            else if (5==convert) {
+                objet1.KelvinVersFarenheit(a);
+            }      
+            else if (6==convert) {
+                objet1.FarenheitVersKelvin(a);
+            } 
+            convert = sc.nextInt();
+        }
+        
+        System.out.println("""
+                           Saisissez les conversions que vous souhaitez effectuer pour objet 2:
+                           1) De Celcius vers Kelvin
+                           2) De Kelvin vers Celcius
+                           3) De Farenheit vers Celcius
+                           4) De Celcius vers Farenheit
+                           5) De Kelvin vers Farenheit
+                           6) De Farenheit vers Kelvin
+                           """);
+        int convert2 = sc.nextInt();
+        while (1<=convert2 && convert2<=6){
+            if (1 == convert2) {
+                objet2.CelciusVersFarenheit(a);
+            }
+            else if (2==convert2) {
+                objet2.KelvinVersCelcius(a);
+            }       
+            else if (3==convert2) {
+                objet2.FarenheitVersCelcius(a);
+            }       
+            else if (4==convert2) {
+                objet2.CelciusVersFarenheit(a);
+            }   
+            else if (5==convert2) {
+                objet2.KelvinVersFarenheit(a);
+            }      
+            else if (6==convert2) {
+                objet2.FarenheitVersKelvin(a);
+            }
+            convert2= sc.nextInt();
+        }
+        
+        System.out.println("Objet1-> "+objet1);
+        System.out.println("Objet2-> "+objet2);
+    
     }
     
 }
