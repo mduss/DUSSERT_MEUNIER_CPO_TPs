e8fe2b9bd11bf87d37019dad40afa3c191628896
dusse <dusse@192.168.1.100>
2021-11-16 21:47:08 +0100

    classes : jeton terminé joueur : terminé cellule : terminé grille : fini jusqu'à tasser grille partie : en cours jusqu'a debuterPartie

--- SP4_console_DUSSERT_MEUNIER/src/sp4_console_dussert_meunier/Partie.java
+++ SP4_console_DUSSERT_MEUNIER/src/sp4_console_dussert_meunier/Partie.java
@@ -10,5 +10,26 @@ package sp4_console_dussert_meunier;
  * @author dusse
  */
 public class Partie {
+    Joueur [] ListeJoueurs=new Joueur[2];
+    Joueur joueurCourant;
+    Grille grilleJeu;
     
+    public Partie(Joueur J1,Joueur J2){
+        
+    }
+    
+    public void attribuerCouleurAuxJoueurs(){
+        ListeJoueurs[1].Couleur="rouge";
+        ListeJoueurs[2].Couleur="jaune";
+    }
+    
+    public void initialiserPartie(){
+        grilleJeu.afficherGrilleSurConsole();
+        ListeJoueurs[1].ListeJetons=new Jeton[21];
+        ListeJoueurs[2].ListeJetons=new Jeton[21];
+    }
+    
+    public void debuterPartie(){
+        
+    }
 }
