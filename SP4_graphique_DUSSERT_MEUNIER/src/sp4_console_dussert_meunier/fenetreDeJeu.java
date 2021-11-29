5680365a2bce09b476cd582b3503a26e6caca8c7
dusse <dusse@192.168.1.100>
2021-11-29 13:12:00 +0100

    partie graphique jusqu'à viédo 3 : erreur affichage

--- SP4_graphique_DUSSERT_MEUNIER/src/sp4_console_dussert_meunier/fenetreDeJeu.java
+++ SP4_graphique_DUSSERT_MEUNIER/src/sp4_console_dussert_meunier/fenetreDeJeu.java
@@ -29,7 +29,6 @@ public class fenetreDeJeu extends javax.swing.JFrame {
         }
     }
 
-    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
