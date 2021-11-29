41a43adb410355f512cbe82d7c0f6528b83f49c0
dusse <dusse@192.168.1.100>
2021-11-29 21:15:37 +0100

    correction tasser grille

--- SP4_console_DUSSERT_MEUNIER/src/sp4_console_dussert_meunier/Grille.java
+++ SP4_console_DUSSERT_MEUNIER/src/sp4_console_dussert_meunier/Grille.java
@@ -136,6 +136,7 @@ public class Grille {
         return false;
     }
     
+    
     public void tasserGrille(int c){
         for (int i=0;i<5;i++){
             if (cellulesJeu[i][c].jetonCourant==null){
