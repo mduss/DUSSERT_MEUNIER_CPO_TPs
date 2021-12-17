16133b83913592ccfac8b5b7b3c16ce69b26a232
dusse <dusse@192.168.1.100>
2021-12-17 09:30:01 +0100

    création class joueur et combinaisons

---
+++ Mastermind_DUSSERT_MEUNIER/src/mastermind_dussert_meunier/Partie.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mastermind_dussert_meunier;
+
+/**
+ *
+ * @author dusse
+ */
+public class Partie {
+    int essai;
+    
+    public void CommencerPartie(){
+        //initialiserCombi();
+        while(combiGagnante()==false && essai<=12){
+            //choisir combi
+            //similitudesPosi
+            //similitudesCoul
+            //afficher console
+            essai++;
+        }
+        if(essai<=12){
+            System.out.println("Vous avez gagné au bout de "+essai+" essais !");
+        }
+        else {
+            System.out.println("Vous avez perdu");
+        }
+    }
+}
