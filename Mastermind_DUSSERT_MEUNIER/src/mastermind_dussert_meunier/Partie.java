401c9b61c110e536d675df2f8dc8718ddb86f241
dusse <dusse@192.168.1.100>
2022-01-02 13:38:29 +0100

    mise à jour de la class partie

--- Mastermind_DUSSERT_MEUNIER/src/mastermind_dussert_meunier/Partie.java
+++ Mastermind_DUSSERT_MEUNIER/src/mastermind_dussert_meunier/Partie.java
@@ -13,55 +13,87 @@ import java.util.Scanner;
  */
 public class Partie {
     int essai;
-    Affichage grille= new Affichage();
+    Affichage grille;
     Combinaisons combiCourante;
     Combinaisons combinaison;
    
-    public void CommencerPartie(){
+    public void CommencerPartieSeul(){
+        grille= new Affichage();
+        essai=0;
         Scanner sc=new Scanner(System.in);
-        combinaison= new Combinaisons(grille.initialiserCombi());
+        combinaison= new Combinaisons(grille.initialiserCombiAléat());
         String [] couleur= new String[4];
+        
         grille.affichageJeu();
+        //choisir combi
         System.out.println("Choisissez votre combinaison de couleurs !");
-        for (int i=0;i<4;i++){
-            System.out.println("Couleur "+i+1+": \nSaisissez : \nro pour rouge \nbl pour bleu \nja pour jaune \nve pour vert \nvi pour violet \nwh pour blanc");
-            sc.reset();
-            sc=new Scanner(System.in);
-            couleur[i]=sc.nextLine();
-            while (!"bl".equals(couleur)&&!"ro".equals(couleur)&&!"ja".equals(couleur)&&!"ve".equals(couleur)&&!"vi".equals(couleur)&&!"wh".equals(couleur)){
-                System.out.println("Saisissez une couelur valide : ");
-                sc.reset();
-                sc=new Scanner(System.in);
-                couleur[i]=sc.nextLine();
-            }
-        }
+        couleur=grille.Combi();
         combiCourante =new Combinaisons(couleur);
+        int posi=grille.SimilitudePosi(combiCourante,combinaison);
+        int coul=grille.SimilitudeCoul(combiCourante,combinaison);
+        grille.tableaux(essai, posi, coul, combiCourante);
         essai++;
-        while(combiCourante.combiGagnante(combiCourante,combinaison)==false && essai<12){
+        
+        while(grille.combiGagnante(combiCourante,combinaison)==false && essai<12){
             grille.affichageJeu();
             //choisir combi
             System.out.println("Choisissez votre combinaison de couleurs !");
-            for (int i=0;i<4;i++){
-                System.out.println("Couleur "+i+1+": \nSaisissez : \nro pour rouge \nbl pour bleu \nja pour jaune \nve pour vert \nvi pour violet \nwh pour blanc");
-                couleur[i]=sc.nextLine();
-                while (!"bl".equals(couleur)&&!"ro".equals(couleur)&&!"ja".equals(couleur)&&!"ve".equals(couleur)&&!"vi".equals(couleur)&&!"wh".equals(couleur)){
-                System.out.println("Saisissez une couelur valide : ");
-                sc.reset();
-                sc=new Scanner(System.in);
-                couleur[i]=sc.nextLine();
-                }
-            }
+            couleur=grille.Combi();
             combiCourante =new Combinaisons(couleur);
-            int posi =combiCourante.SimilitudePosi(combiCourante,combinaison);
-            int coul=combiCourante.SimilitudeCoul(combiCourante,combinaison);
+            posi =grille.SimilitudePosi(combiCourante,combinaison);
+            coul=grille.SimilitudeCoul(combiCourante,combinaison);
             grille.tableaux(essai, posi, coul, combiCourante);
             essai++;
         }
-        if(essai<=12){
-            System.out.println("Vous avez gagné au bout de "+essai+" essais !");
+        grille.tableaux(essai-1, posi, coul, combiCourante);
+        grille.affichageJeu();
+        if(essai<=11){
+            System.out.println("Vous avez trouvé la bonne combianaison au bout de "+essai+" essais !");
         }
         else {
-            System.out.println("Vous avez perdu");
+            System.out.println("Vous avez perdu :'(");
         }
+    }
+    
+    public void CommencerPartie(Joueur j1,Joueur j2){
+        grille= new Affichage();
+        essai=0;
+        Scanner sc=new Scanner(System.in);
+        System.out.println(j2.Nom+" choisissez la combianaison à retrouver sans lui montrer");
+        combinaison= new Combinaisons(grille.Combi());
+        System.out.println(j1.Nom+", c'est à vous de jouer");
+        String [] couleur= new String[4];
+        
+        grille.affichageJeu();
+        //choisir combi
+        System.out.println(j1.Nom+" choisissez votre combinaison de couleurs !");
+        couleur=grille.Combi();
+        combiCourante =new Combinaisons(couleur);
+        int posi=grille.SimilitudePosi(combiCourante,combinaison);
+        int coul=grille.SimilitudeCoul(combiCourante,combinaison);
+        grille.tableaux(essai, posi, coul, combiCourante);
+        essai++;
+        
+        while(grille.combiGagnante(combiCourante,combinaison)==false && essai<12){
+            grille.affichageJeu();
+            //choisir combi
+            System.out.println(j1.Nom+" choisissez votre combinaison de couleurs !");
+            couleur=grille.Combi();
+            combiCourante =new Combinaisons(couleur);
+            posi =grille.SimilitudePosi(combiCourante,combinaison);
+            coul=grille.SimilitudeCoul(combiCourante,combinaison);
+            System.out.print(posi);System.out.print(coul);
+            grille.tableaux(essai, posi, coul, combiCourante);
+            essai++; 
+        }
+        grille.tableaux(essai-1, posi, coul, combiCourante);
+        grille.affichageJeu();
+        if(essai<=11){
+            System.out.println(j1.Nom+", vous avez trouvé la bonne combianaison au bout de "+essai+" essais !");
+        }
+        else {
+            System.out.println(j1.Nom+", vous avez perdu :'(");
+        }
+        j1.essai=essai;
     }
 }
